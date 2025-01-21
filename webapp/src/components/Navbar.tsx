a301f0c73645704ebd6609262379cfd68b33e2d9
NicoletaPa <nicoletap@uia.no>
2025-01-21 12:36:15 +0100

    Update React types and add new layout and page components; remove obsolete Azure Pipelines files

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import shortcuts from '../app/data/shortcuts';
-import { useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
-import Dropdown from './Dropdown';
-import BreakLine from './Breakline';
-import Icons from './Icons';
-import Link from 'next/link';
-import Image from 'next/image';
-import NextBreadcrumb from './BreadCrumbs';
+import shortcuts from "../app/data/shortcuts";
+import { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import Dropdown from "./Dropdown";
+import BreakLine from "./Breakline";
+import Icons from "./Icons";
+import Link from "next/link";
+import Image from "next/image";
+import NextBreadcrumb from "./BreadCrumbs";
 
 const Navbar = () => {
   const [openedNavbarSection, setOpenedNavbarSection] = useState<number>(-1);
@@ -25,36 +25,43 @@ const Navbar = () => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       // Check if the click is outside the navbar
-      if (navbarRef.current && !navbarRef.current.contains(event.target as Node)) {
+      if (
+        navbarRef.current &&
+        !navbarRef.current.contains(event.target as Node)
+      ) {
         setOpenedNavbarSection(-1); // Close the opened section
       }
     };
 
-    document.addEventListener('click', handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
 
     // Lock scrolling if a dropdown is open
     if (openedNavbarSection !== -1) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = '';
+      document.body.style.overflow = "";
     }
 
     return () => {
-      document.removeEventListener('click', handleClickOutside);
-      document.body.style.overflow = ''; // Ensure overflow is reset on cleanup
+      document.removeEventListener("click", handleClickOutside);
+      document.body.style.overflow = ""; // Ensure overflow is reset on cleanup
     };
   }, [openedNavbarSection]); // Add openedNavbarSection as a dependency
 
   return (
     <>
-      <nav ref={navbarRef} className="py-2 sticky top-0 bottom-auto z-50 font-semibold w-full bg-white shadow-xl">
+      <nav
+        ref={navbarRef}
+        className="sticky bottom-auto top-0 z-50 w-full bg-white py-2 font-semibold shadow-xl"
+      >
         {/* Backdrop when a dropdown is open */}
-        {openedNavbarSection !== -1 && ReactDOM.createPortal(
-          <div className="fixed inset-0 bg-black bg-opacity-25 z-10" />,
-          document.body
-        )}
-        <div className="flex justify-between items-center w-full">
-          <div className="flex items-center pl-4 mb-1 lg:pl-10">
+        {openedNavbarSection !== -1 &&
+          ReactDOM.createPortal(
+            <div className="fixed inset-0 z-10 bg-black bg-opacity-25" />,
+            document.body,
+          )}
+        <div className="flex w-full items-center justify-between">
+          <div className="mb-1 flex items-center pl-4 lg:pl-10">
             <Link href={"/"}>
               <Image
                 src="/Kartai-logo_white.jpg" // Path to your image in the public folder
@@ -65,7 +72,16 @@ const Navbar = () => {
               />
             </Link>
           </div>
-          <div className="hidden sm:flex justify-end items-center pr-4 lg:pr-10">
+          <div>
+            <Link
+              href="../atlas-app"
+              className="text-md cursor-pointer text-kartAI-blue"
+            >
+              Atlas-løsning for byggesak
+            </Link>
+          </div>
+
+          <div className="hidden items-center justify-end pr-4 sm:flex lg:pr-10">
             {shortcuts.map((shortcut, index) => {
               const isActive = index === openedNavbarSection;
               return (
@@ -73,35 +89,60 @@ const Navbar = () => {
                   <button
                     data-cy={`dropdown-button-${index}`} // Added data-cy for testing
                     onClick={() => handleClick(index)}
-                    className={`relative group px-2 md:px-8 lg:px-5 py-2 text-md flex flex-row gap-2 cursor-hover items-center ${isActive ? 'text-kartAI-blue' : 'text-secondary-black'}`}
-                    style={isActive ? { textDecoration: 'underline', textUnderlineOffset: '6px', textDecorationThickness: '2px' } : {}}
+                    className={`text-md cursor-hover group relative flex flex-row items-center gap-2 px-2 py-2 md:px-8 lg:px-5 ${isActive ? "text-kartAI-blue" : "text-secondary-black"}`}
+                    style={
+                      isActive
+                        ? {
+                            textDecoration: "underline",
+                            textUnderlineOffset: "6px",
+                            textDecorationThickness: "2px",
+                          }
+                        : {}
+                    }
                   >
-                    <p className="mt-1 whitespace-pre-wrap text-xs xl:text-sm hidden sm:block tracking-[0.15em]">
+                    <p className="mt-1 hidden whitespace-pre-wrap text-xs tracking-[0.15em] sm:block xl:text-sm">
                       {shortcut.header.toUpperCase()}
                     </p>
                   </button>
                   {isActive && (
-                    <div data-cy="dropdown-content" className="fixed left-0 right-0 pt-4 bg-white shadow-lg z-40 w-screen">
+                    <div
+                      data-cy="dropdown-content"
+                      className="fixed left-0 right-0 z-40 w-screen bg-white pt-4 shadow-lg"
+                    >
                       <BreakLine />
                       {/* Dynamically rendering subgroups and links for this section */}
-                      <div className="w-screen-lg flex flex-col mt-4">
-                        <div className="px-10 md:px-20 pb-10 flex flex-wrap">
+                      <div className="w-screen-lg mt-4 flex flex-col">
+                        <div className="flex flex-wrap px-10 pb-10 md:px-20">
                           {shortcut.subgroups.map((subgroup, subgroupIndex) => (
-                            <div key={subgroupIndex} className="w-full md:w-1/2 xl:w-1/3 p-2">
-                              <h3 className="text-xl font-bold mb-2">{subgroup.title}</h3>
-                              <div className={`flex flex-wrap ${subgroup.links.length > 3 ? '' : 'flex-col'}`}> {/* Adjust flex direction based on number of links */}
+                            <div
+                              key={subgroupIndex}
+                              className="w-full p-2 md:w-1/2 xl:w-1/3"
+                            >
+                              <h3 className="mb-2 text-xl font-bold">
+                                {subgroup.title}
+                              </h3>
+                              <div
+                                className={`flex flex-wrap ${subgroup.links.length > 3 ? "" : "flex-col"}`}
+                              >
+                                {" "}
+                                {/* Adjust flex direction based on number of links */}
                                 {subgroup.links.map((link, linkIndex) => (
-                                  <div key={linkIndex} className={`${subgroup.links.length > 3 ? 'w-1/2' : 'w-full'} py-2 pr-4`}>
+                                  <div
+                                    key={linkIndex}
+                                    className={`${subgroup.links.length > 3 ? "w-1/2" : "w-full"} py-2 pr-4`}
+                                  >
                                     <a
                                       data-cy={`link-${subgroupIndex}-${linkIndex}`} // Added data-cy for each link
                                       href={link.url}
-                                      className="text-sm text-black hover:text-gray-600 py-1 flex flex-row gap-4"
+                                      className="flex flex-row gap-4 py-1 text-sm text-black hover:text-gray-600"
                                     >
                                       {link.label}
-                                      {subgroup.arrow && <Icons name="ArrowRight_sm" />}
+                                      {subgroup.arrow && (
+                                        <Icons name="ArrowRight_sm" />
+                                      )}
                                     </a>
                                     {link.text && (
-                                      <a className='text-xs font-light py-1 flex flex-row gap-4'>
+                                      <a className="flex flex-row gap-4 py-1 text-xs font-light">
                                         {link.text}
                                       </a>
                                     )}
@@ -124,10 +165,10 @@ const Navbar = () => {
         </div>
       </nav>
       <NextBreadcrumb
-        homeElement={'Hjem'}
-        separator={'>'}
-        activeClasses=''
-        listClasses='hover:underline mx-2 font-light'
+        homeElement={"Hjem"}
+        separator={">"}
+        activeClasses=""
+        listClasses="hover:underline mx-2 font-light"
       />
     </>
   );
