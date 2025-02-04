1c56887659df9312239b20dcd113f3fc8f8c8c49
mariaathoang <mahoang@uia.no>
2025-02-04 12:23:46 +0100

    fix

--- webapp/src/app/atlas-app/saksbehandler/mottak/mine-saker/dashbord/[caseNumber]/cadaid/page.tsx
+++ webapp/src/app/atlas-app/saksbehandler/mottak/mine-saker/dashbord/[caseNumber]/cadaid/page.tsx
@@ -1,20 +1,16 @@
 import { GeistSans } from "geist/font/sans";
 import CadaidPage from "~/components/CADAiD";
-import { SidebarProvider } from "~/components/ui/sidebar";
-import { AppSidebar } from "~/components/app-sidebar";
+import AtlasSidebar from "~/components/AtlasSidebar";
 
 export default async function PlantegningsAnalyse() {
   return (
-    <div>
-      <SidebarProvider>
-        <AppSidebar />
+        <AtlasSidebar>
 
         <div
           className={`flex min-h-screen pb-40 ${GeistSans.variable} mx-1/3 flex w-full flex-col items-center`}
         >
           <CadaidPage></CadaidPage>
         </div>
-      </SidebarProvider>
-    </div>
+      </AtlasSidebar>
   );
 }
