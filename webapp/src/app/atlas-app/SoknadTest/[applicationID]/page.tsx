cd42e8e5719bd96145fe932c18da4e13fd810f2d
Nicoleta Pavelescu <112115960+Nicoletapa@users.noreply.github.com>
2025-03-28 13:16:41 +0100

    Delete webapp/src/app/atlas-app/SoknadTest/[applicationID]/page.tsx

--- webapp/src/app/atlas-app/SoknadTest/[applicationID]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import TestSoknad from "~/components/TestSoknad";
-
-export default function SoknadTestPage({ params }: { params: { applicationID: string } }) {
-    const applicationID = params.applicationID === "new" ? undefined : parseInt(params.applicationID, 10);
-    return <TestSoknad applicationID={applicationID} />;
-}
